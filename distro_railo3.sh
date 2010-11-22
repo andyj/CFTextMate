ec1dc4419193197eedc8319394544e31a47d59cc
Andy Jarrett <mail@andyjarrett.com>
2010-11-22 17:25:23 +0000

    adding new Railo dicts

---
+++ distro_railo3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ant clean -Doutput.name=Railo-3
+
+ant -Doutput.name=Railo-3 \
+	-Ddictionary.file=Resources/railo3.xml \
+	-Dbundle.uuid=2E937CBE-9B70-42C6-974A-987E8848251E \
+	-Dsyntax.uuid=888FB6E0-4CBC-4521-A51C-3D03AAB85F01 \
+	-Dcmddoc.uuid=632D2425-1F06-4944-9712-7EEA5FE7AE54 \
+	build
+	

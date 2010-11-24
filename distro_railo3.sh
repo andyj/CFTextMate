2f1abd1109daac923a6691e943d9cebb19662a1a
Mark Drew <mark.drew@gmail.com>
2010-11-24 17:20:17 +1100

    updated the XML Resource files the Railo bundle works with

--- distro_railo3.sh
+++ distro_railo3.sh
@@ -3,7 +3,7 @@
 ant clean -Doutput.name=Railo-3.2
 
 ant -Doutput.name=Railo-3.2 \
-	-Ddictionary.file=Resources/railo3.xml \
+	-Ddictionary.file=Resources/railo3.2.xml \
 	-Dxsl.file=railo_dictToBundle.xsl \
 	-Ddoc.xsl.file=railo_dictToAPI.xsl \
 	-Dbundle.uuid=30BE174E-C362-4139-A5AC2145D7584792 \
